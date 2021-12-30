b5db6b05c95a1e5b157f32d7275d9f8b494b60ee
bosley <bosley117@gmail.com>
2021-12-29 22:50:39 -0500

    started analyzer

--- src/compiler/analyzer.cpp
+++ src/compiler/analyzer.cpp
@@ -12,5 +12,11 @@ bool analyzer::analyze() {
   return false;
 }
 
+void analyzer::accept(parse_tree::assignment_statement &stmt) {}
+void analyzer::accept(parse_tree::expression_statement &stmt) {}
+void analyzer::accept(parse_tree::if_statement &stmt) {}
+void analyzer::accept(parse_tree::while_statement &stmt) {}
+void analyzer::accept(parse_tree::for_statement &stmt) {}
+void analyzer::accept(parse_tree::return_statement &stmt) {}
 
 }
--- src/compiler/analyzer.hpp
+++ src/compiler/analyzer.hpp
@@ -3,18 +3,49 @@
 
 #include "parsetree.hpp"
 
-#include<vector>
+#include <vector>
 
 namespace compiler {
 
-class analyzer {
+class analyzer : private parse_tree::visitor {
 public:
   analyzer(std::vector<parse_tree::toplevel_ptr> &parse_trees);
 
   bool analyze();
 
 private:
-  std::vector<parse_tree::toplevel_ptr> &_trees;
+
+  // Flags that need to be true at the end of analyzing 
+  // the parse trees
+  //
+  struct check_flags {
+
+    bool entry_method_exists;
+    
+  };
+
+  //  Information regarding the last function analyzed
+  //
+  struct analyzed_function {
+    std::string file;
+    std::string name;
+    size_t line;
+    size_t col;
+    parse_tree::variable_types indicated_return_type;
+    bool all_exits_match_return_type;
+  };
+
+  std::vector<parse_tree::toplevel_ptr> &_trees; // trees to analyze
+
+  analyzed_function current_function;
+
+
+  virtual void accept(parse_tree::assignment_statement &stmt) override;
+  virtual void accept(parse_tree::expression_statement &stmt) override;
+  virtual void accept(parse_tree::if_statement &stmt) override;
+  virtual void accept(parse_tree::while_statement &stmt) override;
+  virtual void accept(parse_tree::for_statement &stmt) override;
+  virtual void accept(parse_tree::return_statement &stmt) override;
 };
 
 }
